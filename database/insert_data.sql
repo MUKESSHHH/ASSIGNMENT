19bfbb74b92dda1bbff28826c4c34a895872d84b
MUKESH <mukesh.in.tech7@gmail.com>
2025-12-02 14:31:01 +0530

    Create insert_data.sql

---
+++ database/insert_data.sql
@@ -0,0 +1,9 @@
+INSERT INTO Employees VALUES
+(101, 'Aarav', 'Male', 28, 45000, 'HR'),
+(102, 'Priya', 'Female', 32, 52000, 'Finance'),
+(103, 'Rahul', 'Male', 26, 48000, 'IT');
+
+INSERT INTO Department VALUES
+(1, 'HR', 'Mumbai'),
+(2, 'Finance', 'Delhi'),
+(3, 'IT', 'Bangalore');
